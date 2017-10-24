1d462153f4ef799a5b2fd66b222161e74eab8d12
Saara Korhonen <saara.korhonen@courses.tamk.fi>
2017-10-24 10:38:19 +0300

    Juttuja

---
+++ Project2/Assets/Scripts/EnemyScripts/Bullet_line.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_line : MonoBehaviour 
+{
+	GameObject pelaaja;
+	int countLines = 0;
+
+	private LineRenderer[] lines;
+
+	void Awake () 
+	{
+		pelaaja = GameObject.FindGameObjectWithTag ("Player");
+		lines = GetComponentsInChildren<LineRenderer> ();
+
+		//lines [0].enabled = false;
+		lines [1].enabled = false;
+		lines [2].enabled = false;
+
+		//InvokeRepeating ("StartAttack", 1,1);
+	}
+
+	void Update ()
+	{
+		lines [0].SetPosition (1, pelaaja.transform.position);
+	}
+
+	void StartAttack () 
+	{
+		if (countLines < lines.Length) 
+		{
+			Invoke ("Attack", 0);
+		}
+	}
+
+	void Attack ()
+	{
+		lines [countLines].enabled = true;
+		countLines++;
+	}
+}
