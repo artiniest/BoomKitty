b3925e98d288058c72ca6674e1e84e8b1696eb7b
Saara Korhonen <saara.korhonen@courses.tamk.fi>
2017-10-26 15:22:15 +0300

    AHWEN

---
+++ Project2/Assets/Scripts/EnemyScripts/Ahwen/Attack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour 
+{
+	void SMALLSHAKE ()
+	{
+		ShakeScreen.shakeAmount = 0.1f;
+		ShakeScreen.shakeDuration = 2f;
+	}
+
+	void MEDIUMSHAKE ()
+	{
+		ShakeScreen.shakeAmount = 0.2f;
+		ShakeScreen.shakeDuration = 2f;
+	}
+
+	void BIGSHAKE ()
+	{
+		ShakeScreen.shakeAmount = 0.7f;
+		ShakeScreen.shakeDuration = 5;
+	}
+}
