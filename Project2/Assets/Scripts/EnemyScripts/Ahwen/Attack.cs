1da698645decfc96695e5e9069d22c2cf8f90d32
Saara Korhonen <saara.korhonen@courses.tamk.fi>
2017-10-26 16:27:30 +0300

    Ahwen-juttuja

--- Project2/Assets/Scripts/EnemyScripts/Ahwen/Attack.cs
+++ Project2/Assets/Scripts/EnemyScripts/Ahwen/Attack.cs
@@ -4,6 +4,27 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour 
 {
+	public int healthPoints = 3000;
+	public Animator maattori;
+
+	void Start ()
+	{
+		maattori = GetComponent<Animator> ();
+	}
+
+	void Update ()
+	{
+		if (healthPoints < 2950) 
+		{
+			maattori.SetBool ("hasTakenDmg", true);
+		}
+
+		if (healthPoints < 2900) 
+		{
+			maattori.SetBool ("isThisHisFinalForm", true);
+		}
+	}
+
 	void SMALLSHAKE ()
 	{
 		ShakeScreen.shakeAmount = 0.1f;
@@ -20,5 +41,13 @@ public class Attack : MonoBehaviour
 	{
 		ShakeScreen.shakeAmount = 0.7f;
 		ShakeScreen.shakeDuration = 5;
+	}
+
+	void OnTriggerEnter2D (Collider2D other) 
+	{
+		if (other.tag == "Bullet") 
+		{
+			healthPoints--;
+		}
 	}
 }
